8a36cfab813583c35fb421851ac68aa966086b2e
Pedro Wenner <pedro_wenner@outlook.com>
2026-03-24 22:35:15 -0400

    feat: adição de migração para rotina de cálculo em rubricas

---
+++ DPManagement.Infrastructure/Migrations/20260325023445_AddRotinaCalculoToRubrica.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DPManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRotinaCalculoToRubrica : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Rotina",
+                table: "Rubricas",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rotina",
+                table: "Rubricas");
+        }
+    }
+}
